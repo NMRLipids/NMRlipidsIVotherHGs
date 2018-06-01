98bed738ed7b5f1ff025e2069df39d784f8d31c7
ohsOllila <samuli.ollila@helsinki.fi>
2018-06-01 11:43:22 +0300

    Update and correct Lipid17 plots

--- scripts/grepOPdata.sh
+++ scripts/grepOPdata.sh
@@ -1,7 +1,7 @@
-grep alpha1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep alpha2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep beta1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep beta2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep alpha1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPS.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep alpha2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPS.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
-grep beta ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc+4000mMKCl/T298/MODEL_LIPID17/OrdParsPOPS.dat | awk '{print $2"  "$3"   "}' 
+grep alpha1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep alpha2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep beta1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep beta2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPC.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep alpha1 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPS.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep alpha2 ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPS.dat | awk '{print $2"  "$3"  "}' | tr -d "\n"
+grep beta ../../MATCH/Data/Lipid_Bilayers/POPS+83%popc/T298K/MODEL_LIPID17_POTASSIUMcounterion/OrdParsPOPS.dat | awk '{print $2"  "$3"   "}' 
